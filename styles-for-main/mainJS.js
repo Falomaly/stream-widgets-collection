ef7a5bc97a10629da228bf3ca96fc40932df3177
wwc <Falomaly9@gmail.com>
2023-01-26 11:14:30 +0200

    - Catalogue menu proves. - Some animations.

--- styles-for-main/mainJS.js
+++ styles-for-main/mainJS.js
@@ -1,8 +1,11 @@
 window.onload = function (event) {
+    
     let $chinaechiAfkBanner = document.querySelector("#afk-banner-chinaechi")
 
-    $chinaechiAfkBanner.addEventListener("click", () => {
-
+    $chinaechiAfkBanner.addEventListener("click", (event) => {
+        if (event.target.tagName === "A") {
+            return
+        }
         if ($chinaechiAfkBanner) {
             window.open("https://falomaly.github.io/stream-widgets-collection/Chinaechi-widgets/afk-banner-widget/", "_blank")
         }
@@ -11,7 +14,11 @@ window.onload = function (event) {
 
     let $rasputinAfkBanner = document.querySelector("#afk-banner-rasputin")
 
-    $rasputinAfkBanner.addEventListener("click", () => {
+    $rasputinAfkBanner.addEventListener("click", (event) => {
+
+        if (event.target.tagName === "A") {
+            return
+        }
 
         if ($rasputinAfkBanner) {
             window.open("https://falomaly.github.io/stream-widgets-collection/Rasputin322-widgets/afk-banner-widget/", "_blank")
